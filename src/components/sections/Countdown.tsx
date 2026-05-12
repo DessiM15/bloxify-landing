309b7de7cffda0972d2763032d1ce113d13edd5d
DessiM15 <dessiahd1@gmail.com>
2026-05-12 05:27:34 -0500

    Align Blox vertical position with countdown blocks instead of section center

--- src/components/sections/Countdown.tsx
+++ src/components/sections/Countdown.tsx
@@ -450,7 +450,7 @@ export default function Countdown() {
 
       {/* Blox in the left empty space — desktop only. Hidden on launch. */}
       {!launched && (
-        <div className="hidden lg:block absolute left-0 xl:left-8 top-1/2 -translate-y-1/2 w-[448px] h-[448px] pointer-events-none z-0">
+        <div className="hidden lg:block absolute left-0 xl:left-8 top-[100px] w-[448px] h-[448px] pointer-events-none z-0">
           <Image
             src="/images/mascot/blox_presenting-removebg-preview.png"
             alt="Blox presenting"
